4fd85d079a426269d6a4fa11738dc52cb0745370
Your Name <vudai12341@gmail.com>
2021-11-07 22:23:06 +0700

    10.22pm/07.11.2021

--- bai1/src/read_numbers.java
+++ bai1/src/read_numbers.java
@@ -1,14 +1,50 @@
-////import java.util.Scanner;
-////
-////public class read_numbers {
-////    public static void main(String[] args) {
-////        Scanner scanner = new Scanner(System.in);
-////        System.out.println("nhập số bất kỳ: ");
-////        int number = scanner.nextInt();
-////        int sum = 0;
-////        if (number > 100 && number < 1000){
-////            sum = number% 100;
-////            System.out.println(sum);
-////        }
-//    }
-//}
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class read_numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input any number: ");
+        int num = scanner.nextInt();
+        String[] temp = Integer.toString(num).split("");
+        System.out.println(Arrays.toString(temp));//xác nhận biến thành mảng
+        for (String a : temp) {
+            number(a);
+        }
+    }
+
+    public static void number(String num) {
+        switch (num) {
+            case "1":
+                System.out.print("Một ");
+                break;
+            case "2":
+                System.out.print("Hai ");
+                break;
+            case "3":
+                System.out.print("Ba ");
+                break;
+            case "4":
+                System.out.print("Bốn ");
+                break;
+            case "5":
+                System.out.print("Năm ");
+                break;
+            case "6":
+                System.out.print("Sáu ");
+                break;
+            case "7":
+                System.out.print("Bảy ");
+                break;
+            case "8":
+                System.out.print("Tám ");
+                break;
+            case "9":
+                System.out.print("Chín ");
+                break;
+            case "0":
+                System.out.println("Không ");
+                break;
+        }
+    }
+}
